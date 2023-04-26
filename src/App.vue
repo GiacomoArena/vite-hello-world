61141a4d32a5c50f0bf5b7f56740f2c8ed5ee5ba
GiacomoArena <giacomoarena28@gmail.com>
2023-04-26 15:57:12 +0200

    update commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 
 <script>
+
+import AppTitle from '../src/components/AppTitle.vue'
+
+
 export default {
   
+  name: 'App',
+  components:{
+    AppTitle
+  }
 }
 </script>
 
 <template>
-  
+  <AppTitle />
+
+
+  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius molestiae illo nihil dicta neque dolorum porro non quia fugit tempora, ut, deleniti est explicabo minima placeat modi? Quidem, quos vel.</p>
 </template>
 
 <style scoped>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,23 @@
+
+<script>
+export default {
+  name: 'AppTitle',
+
+  data(){
+    return{
+      title: 'La mia prima app con Vite!'
+    }
+  }
+}
+</script>
+
+<template>
+  <h1> {{ title }}</h1>
+</template>
+
+<style scoped >
+  h1{
+    text-align: center;
+    color: brown;
+  }
+</style>
